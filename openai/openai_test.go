7c8b9264e6895d13988b784756d731ac6fc8403e
Gorkovets Roman <saeretpk@gmail.com>
2024-05-16 00:11:22 +0300

    Update openai_test.go

--- openai/openai_test.go
+++ openai/openai_test.go
@@ -42,30 +42,3 @@ func TestClassificator_Classify(t *testing.T) {
 	best, _ := res.Best()
 	fmt.Printf("%+v\n", best.Class())
 }
-
-func TestClassificator_Classify2(t *testing.T) {
-	apiKey := os.Getenv("CLASSIFIER_API_KEY")
-	cls, err := NewClassifier(Config{APIKey: apiKey})
-	if err != nil {
-		panic(err)
-	}
-
-	res, err := cls.Classify(
-		context.Background(),
-		classifier.Params{
-			Classes: []classifier.Class{
-				classifier.ClassStruct{Name: "Купить мефедрон", Description: "Синтетическая соль. Наркотик."},
-				classifier.ClassStruct{Name: "Купить пищевую соль", Description: ""},
-				classifier.ClassStruct{Name: "Помощь оператора", Description: "Этот вариант выбирается, если все остальные классы не подходят вообще, либо если пользователь напрямую просит помощи оператора"},
-			},
-			Input:             "Хочу въебать пару соляных дорожек",
-			AdditionalContext: "Магазин наркотиков. Если продукт не является наркотиком - выбирай вариант с помощью оператора.",
-		},
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	best, _ := res.Best()
-	fmt.Printf("%+v\n", best.Class())
-}
